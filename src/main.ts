273be06ae9401e7f59b440a7dc3491e3cab4016f
Bao Trinh <qubidt@gmail.com>
2024-11-12 06:50:12 -0600

    feat: initialize plugin from template

---
+++ src/main.ts
@@ -0,0 +1,60 @@
+import { type App, Plugin, PluginSettingTab, Setting } from "obsidian";
+import { t } from "./i18n";
+
+// Remember to rename these classes and interfaces!
+
+interface MyPluginSettings {
+	mySetting: string;
+}
+
+const DEFAULT_SETTINGS: MyPluginSettings = {
+	mySetting: "default",
+};
+
+export default class MyPlugin extends Plugin {
+	settings: MyPluginSettings;
+
+	async onload() {
+		console.debug("loading Sample Plugin");
+		await this.loadSettings();
+		this.addSettingTab(new SampleSettingTab(this.app, this));
+	}
+
+	onunload() {}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
+
+class SampleSettingTab extends PluginSettingTab {
+	plugin: MyPlugin;
+
+	constructor(app: App, plugin: MyPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const { containerEl } = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName(t("setting.option-setting1"))
+			.setDesc(t("setting.option-setting1-desc"))
+			.addText((text) =>
+				text
+					.setPlaceholder(t("setting.option-setting1-placeholder"))
+					.setValue(this.plugin.settings.mySetting)
+					.onChange(async (value) => {
+						this.plugin.settings.mySetting = value;
+						await this.plugin.saveSettings();
+					}),
+			);
+	}
+}
