4fad12ee763dd7d8d83f64afd2f780c77f5c6cf1
Bao Trinh <qubidt@gmail.com>
2024-11-13 19:11:22 -0600

    feat: add logging

--- src/timestampy.ts
+++ src/timestampy.ts
@@ -9,18 +9,18 @@ export default class Timestampy extends Plugin {
 	settings: TimestampySettings;
 
 	async onload() {
-		console.debug("loading Timestampy plugin");
+		console.log("loading Timestampy plugin");
 		await this.loadSettings();
 		this.addSettingTab(new TimestampySettingTab(this.app, this));
 	}
 
-	onunload() {}
-
 	async loadSettings() {
+		console.debug("loading Timestampy settings");
 		this.settings = { ...DEFAULT_SETTINGS, ...(await this.loadData()) };
 	}
 
 	async saveSettings() {
+		console.debug("saving Timestampy settings");
 		await this.saveData(this.settings);
 	}
 }
