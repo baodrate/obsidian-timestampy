c7f5154f43a662a87f01c0296cf69adfd37cbe83
Bao Trinh <qubidt@gmail.com>
2025-05-05 13:41:37 -0500

    chore: upgrade obsidian-typings

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,6 +2,6 @@ import * as en from "./locale/en.json";
 
 const NAMESPACE = "plugin-timestampy";
 
-i18next.addResourceBundle("en", NAMESPACE, en);
+window.i18next.addResourceBundle("en", NAMESPACE, en);
 
-export const t = i18next.getFixedT(null, NAMESPACE);
+export const t = window.i18next.getFixedT(null, NAMESPACE);
