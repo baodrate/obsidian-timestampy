4e7c8b47caeb1218a3541475a4c867ecfef9d5d3
Bao Trinh <qubidt@gmail.com>
2025-06-05 04:24:34 -0500

    TMP: settings proxy

--- src/settings.ts
+++ src/settings.ts
@@ -1,23 +1,98 @@
-import { type App, PluginSettingTab, Setting } from "obsidian";
+import {
+	type App,
+	PluginSettingTab,
+	Setting, TextComponent,
+} from "obsidian";
 import { t } from "./i18n";
 import type TimestampyPlugin from "./timestampy";
+import {TFunction} from "i18next";
+
+export type Nullable<T> = {
+	[K in keyof T]: T[K] | null;
+};
+
+type ValuesOf<T> = T[keyof T];
+
+// to workaround unimplemented feature: https://github.com/microsoft/TypeScript/issues/48992
+type KeysMatching<T, V> = {
+	[K in keyof T]-?: T[K] extends V ? K : never;
+}[keyof T];
+
+type Expand<T> = T extends infer O ? { [K in keyof O]: O[K] } : never;
+type ExpandRecursively<T> = T extends object ? T extends infer O ? { [K in keyof O]: ExpandRecursively<O[K]>}: never : T;
 
 export interface TimestampySettings {
 	timestampFormat: string;
 	createdProperty: string;
 	modifiedProperty: string;
+	num: number;
+	updateOnWrite: boolean;
 }
 
-export const GLOBAL_SETTINGS: TimestampySettings = {
+const GLOBAL_SETTINGS: Readonly<TimestampySettings> = {
 	timestampFormat: "YYYY-MM-DD HH:mm:ss",
 	createdProperty: "created",
 	modifiedProperty: "updated",
+	num: 2384,
+	updateOnWrite: true,
 };
 
-export const DEFAULT_SETTINGS: TimestampySettings = {
-	timestampFormat: GLOBAL_SETTINGS.timestampFormat,
-	createdProperty: "",
-	modifiedProperty: "",
+function Nulled<T extends Record<string, any>>(obj: T): Nullable<T> {
+	return Object.fromEntries(
+		Object.entries(obj).map(([key, _value]) => [key, null]),
+	) as Nullable<T>;
+}
+
+export const DEFAULT_SETTINGS: Readonly<Nullable<TimestampySettings>> = {
+	...Nulled(GLOBAL_SETTINGS),
+	num: 88,
+};
+
+export const SettingsProxyHandler: ProxyHandler<TimestampySettings> = {
+	get: (target, prop) => {
+		return (
+			target[prop as keyof TimestampySettings] ??
+			GLOBAL_SETTINGS[prop as keyof TimestampySettings]
+		);
+	},
+	set: (
+		target: TimestampySettings,
+		prop: keyof typeof target,
+		value: (typeof target)[typeof prop] | null,
+		_receiver,
+	) => {
+		console.log("setting", prop, value);
+		if (value == null) {
+			(target as Nullable<typeof target>)[prop] = value;
+			return true;
+		}
+		if (typeof GLOBAL_SETTINGS[prop] === "string") {
+			const k = prop as KeysMatching<typeof target, string>;
+			const v = value as string | null;
+			// convert empty string to null
+			(target as Nullable<typeof target>)[k] = v?.trim() ? v : null;
+			console.log("target", target);
+			return true;
+		}
+		const v = value as Exclude<typeof value, string>;
+		const k = prop as KeysMatching<typeof target, typeof v>;
+		target[k] = v;
+		return true;
+	},
+	getOwnPropertyDescriptor(target, prop) {
+		return (
+			Object.getOwnPropertyDescriptor(target, prop) ??
+			Object.getOwnPropertyDescriptor(GLOBAL_SETTINGS, prop)
+		);
+	},
+	ownKeys(target) {
+		return [
+			...new Set([...Object.keys(GLOBAL_SETTINGS), ...Object.keys(target)]),
+		];
+	},
+	has(target, prop) {
+		return prop in target || prop in GLOBAL_SETTINGS;
+	},
 };
 
 export class TimestampySettingTab extends PluginSettingTab {
@@ -26,6 +101,19 @@ export class TimestampySettingTab extends PluginSettingTab {
 	constructor(app: App, plugin: TimestampyPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
+	}
+
+	private setPlaceholderAndValue<T extends TextComponent>(component: T, k: KeysMatching<TimestampySettings, string>): T {
+		component.setPlaceholder(GLOBAL_SETTINGS[k]);
+		if (this.plugin.userSettings[k] !== null) {
+			component.setValue(this.plugin.userSettings[k]);
+		}
+		component
+			.onChange(async (value) => {
+				this.plugin.settings[k] = value;
+				await this.plugin.saveSettings();
+			});
+		return component;
 	}
 
 	display(): void {
@@ -66,44 +154,19 @@ export class TimestampySettingTab extends PluginSettingTab {
 						}),
 					);
 
-					// mf.setPlaceholder(GLOBAL_SETTINGS.timestampFormat);
-					mf.setDefaultFormat(GLOBAL_SETTINGS.timestampFormat);
-
-					if (this.plugin.settings.timestampFormat) {
-						mf.setValue(this.plugin.settings.timestampFormat);
-					}
-
-					mf.onChange(async (value) => {
-						this.plugin.settings.timestampFormat = value;
-						await this.plugin.saveSettings();
-					});
+					this.setPlaceholderAndValue(mf, "timestampFormat")
+						.setDefaultFormat(GLOBAL_SETTINGS.timestampFormat);
 				});
 			});
 
 		new Setting(containerEl)
 			.setName(t("setting.created-property.name"))
 			.setDesc(t("setting.created-property.desc"))
-			.addText((text) =>
-				text
-					.setPlaceholder(GLOBAL_SETTINGS.createdProperty)
-					.setValue(this.plugin.settings.createdProperty)
-					.onChange(async (value) => {
-						this.plugin.settings.createdProperty = value;
-						await this.plugin.saveSettings();
-					}),
-			);
+			.addText((text) => this.setPlaceholderAndValue(text, "createdProperty"));
 
 		new Setting(containerEl)
 			.setName(t("setting.modified-property.name"))
 			.setDesc(t("setting.modified-property.desc"))
-			.addText((text) =>
-				text
-					.setPlaceholder(GLOBAL_SETTINGS.modifiedProperty)
-					.setValue(this.plugin.settings.modifiedProperty)
-					.onChange(async (value) => {
-						this.plugin.settings.modifiedProperty = value;
-						await this.plugin.saveSettings();
-					}),
-			);
+			.addText((text) => this.setPlaceholderAndValue(text, "modifiedProperty"));
 	}
 }
--- src/timestampy.ts
+++ src/timestampy.ts
@@ -1,14 +1,18 @@
 import { Plugin } from "obsidian";
 import {
 	DEFAULT_SETTINGS,
+	SettingsProxyHandler,
 	type TimestampySettings,
+	type Nullable,
 	TimestampySettingTab,
 } from "./settings";
 
 export default class TimestampyPlugin extends Plugin {
 	declare settings: TimestampySettings;
+	declare userSettings: Nullable<TimestampySettings>;
 
 	async onload() {
+		console.debug("=============");
 		console.debug("loading Timestampy plugin");
 		await this.loadSettings();
 		this.addSettingTab(new TimestampySettingTab(this.app, this));
@@ -19,10 +23,21 @@ export default class TimestampyPlugin extends Plugin {
 	}
 
 	async loadSettings() {
-		this.settings = { ...DEFAULT_SETTINGS, ...(await this.loadData()) };
+		console.log("loading Timestampy settings");
+		const loadedSettings = await this.loadData();
+		this.userSettings = Object.assign({}, DEFAULT_SETTINGS, loadedSettings);
+		// @ts-ignore: can't use different getter/setter types with Proxy; see: https://github.com/microsoft/TypeScript/issues/43826
+		this.settings = new Proxy<TimestampySettings>( this.userSettings, SettingsProxyHandler);
+		console.log("load this.settings", this.settings);
+		console.log("load json.settings", JSON.stringify(this.settings));
+		console.log("load this._settings", JSON.stringify(this.userSettings));
+		// console.log("setting getOwnPropertyNames:", Object.getOwnPropertyNames(this.settings));
 	}
 
 	async saveSettings() {
-		await this.saveData(this.settings);
+		console.log("save this.settings", this.settings);
+		console.log("save json.settings", JSON.stringify(this.settings));
+		console.log("save this._settings", JSON.stringify(this.userSettings));
+		await this.saveData(this.userSettings);
 	}
 }
